f14842941b4bb4334e5f625b0b6850fa0f01c21e
Nick Cox <nick@nickcox.me>
2015-03-01 09:10:11 -0800

    update capybara tests

--- spec/features/create_link_spec.rb
+++ spec/features/create_link_spec.rb
@@ -2,10 +2,22 @@ require 'spec_helper'
 require 'load_rails'
 
 describe 'creating a link', type: :feature, js: true do
-  it 'creates a link successfully' do
-    visit '/'
-    fill_in 'link-input', with: 'http://google.com'
-    click_button 'Go'
-    expect(page).to have_content 'Your link is'
+  context 'happy path' do
+    it 'creates a link successfully' do
+      visit '/'
+      fill_in 'link-input', with: 'http://google.com'
+      click_button 'Go'
+      expect(page).to have_content 'Your link is'
+      expect(page).to have_content Link.last.slug
+    end
+  end
+
+  context 'errors' do
+    it 'displays an error message' do
+      visit '/'
+      fill_in 'link-input', with: 'kittens'
+      click_button 'Go'
+      expect(page).to have_content I18n.t('models.link.errors.url')
+    end
   end
 end
